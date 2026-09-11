7edb8186c675e018e81a9f5d12cb21ad389eb0a7
bfbcsongs <paulm4035@gmail.com>
2026-09-12 05:43:47 +0800

    Upgrade service worker to version 2
    Updated service worker to version 2 and improved cache handling.

--- sw.js
+++ sw.js
@@ -1,38 +1,19 @@
-const CACHE_NAME = 'sales-calc-v1';
+const CACHE_NAME = 'sales-calc-v2'; // <--- Palitan ang version number dito (e.g. v1 to v2)
 
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Agad na gagamitin ang bagong bersyon
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(cachedResponse => {
-        return cachedResponse || fetch(event.request);
-      })
-  );
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache); // Binubura ang lumang cache
+                    }
+                })
+            );
+        })
+    );
 });
